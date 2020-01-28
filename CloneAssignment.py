cfd686d88238edcc774d63a085cd77d764a3d245
Ata Fatahi <immrata@gmail.com>
2020-01-27 19:12:11 -0500

    added support for specifying out dir

--- CloneAssignment.py
+++ CloneAssignment.py
@@ -3,12 +3,13 @@
 import os
 import sys
 
-if(len(sys.argv) == 4):
+if(len(sys.argv) == 5):
     classroomName = sys.argv[1]
     assignmentName = sys.argv[2]
     rosterPath = sys.argv[3]
+    outpath = sys.argv[4]
 else:
-    print ("Invalid command line arguments, please run CloneAssignment.py [name of classroom][name of assignment] [path to roster]")
+    print ("Invalid command line arguments, please run CloneAssignment.py [name of classroom][name of assignment] [path to roster] [absolute path for out dir]")
     sys.exit(0)
 
 with open(rosterPath) as f:
@@ -16,5 +17,5 @@ with open(rosterPath) as f:
 
 for name in names:
     print(name)
-    cloneCmd = "git clone git@github.com:{}/{}-{}".format(classroomName, assignmentName, name)
+    cloneCmd = "git clone git@github.com:{}/{}-{}".format(classroomName, assignmentName, name) + " " + outpath+ "{}-{}".format(assignmentName, name)
     os.system(cloneCmd)
